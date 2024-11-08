1a5c6363901c722dabc465984d6c9b807c04dae5
SrTorres07 <srtorresfelipe07@outlook.com>
2024-11-08 14:22:02 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = ListElements => {
+    let element = "";
+    element = ListElements[Math.floor(Math.random() * ListElements.length)];
+    return element;
+  };
+
+  let excuseGen = document.querySelector("#excuse");
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    "" +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    "" +
+    getRandomElement(when);
 };
